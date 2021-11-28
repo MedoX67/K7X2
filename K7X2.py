374b02c6ce9da730d99d7d50ca260e8b09fda06a
MedoX67 <93802008+MedoX67@users.noreply.github.com>
2021-11-28 21:57:23 +0200

    Add files via upload
    Install 
    $ pip install K7X2

---
+++ K7X2.py
@@ -0,0 +1,15 @@
+import os
+os.system ('clear')
+
+def v7x (Your_Name) :
+    print ("Welcome Back", Your_Name)
+
+def v7x1 (one,two) :
+    print (one*two)
+
+def v7x2 (name) :
+    name = input (" Enter Your Name: ")
+    if name == 'Ahmed' :
+        print ("Welcome...", name)
+    else:
+        print ('Error' *1000)
